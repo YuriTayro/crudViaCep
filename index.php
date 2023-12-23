56d8efcf46039c1064175f573685352bbb9a6bb4
YuriTayro <yuri.tayrocnp@gmail.com>
2023-12-22 22:46:49 -0300

    novas implementações no index.php e index.html

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
-if(isset($_GET['nom'])) {
-    echo $_GET['nom'];
-} else {
-    echo "Campo 'nom' não foi enviado no formulário.";
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['cep'])) {
+    // Processar dados do formulário, se necessário
+} else if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['cep'])) {
+    $cep = $_GET['cep'];
+    $cep = preg_replace('/\D/', '', $cep); // Remove caracteres não numéricos
+
+    if (strlen($cep) == 8) {
+        $url = "https://viacep.com.br/ws/{$cep}/json/";
+        $response = file_get_contents($url);
+        $data = json_decode($response, true);
+
+        if (!$data || isset($data['erro'])) {
+            echo json_encode(['error' => 'CEP não encontrado. Por favor, verifique o CEP digitado.']);
+        } else {
+            echo json_encode($data);
+        }
+    } else {
+        echo json_encode(['error' => 'Formato de CEP inválido.']);
+    }
 }
 ?>
